412e0e85f938d7747bd6508b45a89c4372feffee
Joao Andrade Jr <fullservicemanaus@gmail.com>
2023-05-03 15:46:33 -0300

    feat: req 01

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import './App.css';
 class App extends React.Component {
   render() {
     return (
-      <h1>Exercise - Pokedex</h1>
+      <h1>Exercício Pokédex</h1>
     );
   }
 }
---
+++ src/components/Pokemon.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+export default class Pokemon extends Component {
+  render() {
+    console.log(this.props)
+    const { pokemon: { name, type, image,
+      averageWeight: { value, measurementUnit } }
+    } = this.props;
+    return (
+      <li>
+        <h4>{ name }</h4>
+        <h4>{ type }</h4>
+        <h4>{ `${value} ${measurementUnit}` }</h4>
+        <img src={ image } alt={ name } />
+      </li>
+    );
+  }
+}
